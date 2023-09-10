edc65e5a8b2bbf0adc19ae89dfaaab3ccbbb4205
Alexander Gonzalez <alexfertel97@gmail.com>
2023-09-10 10:58:35 +0200

    fix(tests): use .t.sol extension for test data

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-#![doc = include_str!("../README.md")]
+// FIXME: Fix doctests in the README.
+// #![doc = include_str!("../README.md")]
 #![warn(missing_docs, unreachable_pub, unused, rust_2021_compatibility)]
 #![warn(clippy::all, clippy::pedantic, clippy::cargo, clippy::nursery)]
 pub mod check;
--- tests/scaffold.rs
+++ tests/scaffold.rs
@@ -4,35 +4,58 @@ use std::{
     process::{Command, Output},
 };
 
+use owo_colors::OwoColorize;
 use pretty_assertions::assert_eq;
 
-fn scaffold(tree_path: &PathBuf, binary_path: &PathBuf) -> Output {
+fn get_trees(cwd: &PathBuf) -> Vec<PathBuf> {
+    let trees = fs::read_dir(cwd.join("tests/trees")).expect("should read the trees directory");
+    let trees: Vec<PathBuf> = trees.filter_map(|entry| Some(entry.ok()?.path())).collect();
+    trees
+}
+
+fn scaffold(binary_path: &PathBuf, tree_path: &PathBuf, args: &[&str]) -> Output {
     Command::new(binary_path)
         .arg("scaffold")
         .arg(tree_path)
+        .args(args)
         .output()
         .expect("should execute the scaffold command")
 }
 
 #[test]
-fn test_scaffold_trees() {
+fn scaffolds_trees() {
     let cwd = env::current_dir().unwrap();
-    let trees = fs::read_dir(cwd.join("tests/trees")).expect("should read the trees directory");
-    let trees: Vec<PathBuf> = trees.filter_map(|entry| Some(entry.ok()?.path())).collect();
-
+    let trees = get_trees(&cwd);
     let binary_path = cwd.join("target/debug/bulloak");
 
     for tree_path in trees {
         if tree_path.extension().unwrap() == "tree" {
-            let output = scaffold(&tree_path, &binary_path);
+            let output = scaffold(&binary_path, &tree_path, &[]);
             let actual = String::from_utf8(output.stdout).unwrap();
 
             let mut output_file = tree_path.clone();
-            output_file.set_extension("sol");
+            output_file.set_extension("t.sol");
             let expected = fs::read_to_string(output_file).unwrap();
 
             // We trim here because we don't care about ending newlines.
             assert_eq!(expected.trim(), actual.trim());
+        }
+    }
+}
+
+#[test]
+fn skips_trees_when_file_exists() {
+    let cwd = env::current_dir().unwrap();
+    let trees = get_trees(&cwd);
+    let binary_path = cwd.join("target/debug/bulloak");
+
+    for tree_path in trees {
+        if tree_path.extension().unwrap() == "tree" {
+            let output = scaffold(&binary_path, &tree_path, &["-w"]);
+            let actual = String::from_utf8(output.stderr).unwrap();
+
+            let expected = format!("{}", "WARN".yellow());
+            assert!(actual.starts_with(&expected));
         }
     }
 }
