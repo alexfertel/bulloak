d262cf70e8f5c93a62f07ce6d5bbf160d64c6043
Giovanni Di Siena <giodisiena@gmail.com>
2024-02-22 14:21:17 +0000

    check tests

---
+++ tests/check/contract_names_mismatch_multiple_roots.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.0;
+
+contract ContractName {
+  function test_ShouldHaveANameMismatchInTheContracts1() external {
+    // It should match the result of `keccak256(abi.encodePacked(a,b))`.
+  }
+  
+  function test_ShouldHaveANameMismatchInTheContracts2() external {
+    // It should match the result of `keccak256(abi.encodePacked(a,b))`.
+  }
+}
