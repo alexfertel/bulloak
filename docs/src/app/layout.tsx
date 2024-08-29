b78fdacb5b3ee3a3ead8f6a23e77b394878962eb
Alexander Gonzalez <alexfertel97@gmail.com>
2024-08-29 16:10:26 +0200

    feat(docs): add analytics

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { body, mono } from "./fonts";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
   title: "bulloak - Test Generator using Branching Tree Technique",
-  description: "bulloak is a powerful test generator that implements the Branching Tree Technique (BTT) for comprehensive smart contract testing.",
-  keywords: "bulloak, Solidity, test generator, Branching Tree Technique, BTT, smart contracts, Ethereum",
+  description:
+    "bulloak is a powerful test generator that implements the Branching Tree Technique (BTT) for comprehensive smart contract testing.",
+  keywords:
+    "bulloak, Solidity, test generator, Branching Tree Technique, BTT, smart contracts, Ethereum",
 };
 
 export default function RootLayout({
@@ -19,9 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body 
-        className={`${mono.className} antialiased touch-manipulation`}
-      >{children}</body>
+      <body className={`${mono.className} antialiased touch-manipulation`}>
+        {children}
+
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
