aec20379a3d81cc58502cad2cf83184574236243
Alexander Gonzalez <alexfertel97@gmail.com>
2024-08-29 15:58:09 +0200

    fix: remove unnecessary assets

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import AsciiTreeAnimation from "../components/AsciiTreeAnimation";
+import AsciiTreeAnimation from "../components/TreesAnimation";
 
 export default function Home() {
   return (
