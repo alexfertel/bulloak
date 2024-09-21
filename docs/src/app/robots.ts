a518c02825efc0e47069a04162d053b830c0c6be
Alexander Gonzalez <alexfertel97@gmail.com>
2024-09-21 12:06:33 +0200

    fix(docs): settle for directly adding a opengraph-image.png

--- docs/src/app/robots.ts
+++ docs/src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        allow: ["/", "/api/og/*"],
+        allow: "/",
       },
     ],
     sitemap: `${url}/sitemap.xml`,
