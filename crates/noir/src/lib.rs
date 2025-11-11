f6fe0d1cf3b68e6b0da7d8b176ac2a369a40406f
teddy <teddy@defi.sucks>
2025-11-11 10:20:55 -0300

    docs: barebone docs for new backend

--- crates/noir/src/lib.rs
+++ crates/noir/src/lib.rs
@@ -1,7 +1,16 @@
+//! A `bulloak` backend for aztec-noir tests.
+//!
+//! `bulloak-noir` provides an implementation of turning a `bulloak-syntax`
+//! AST into a `_test.nr` file containing scaffolded and ready-to-run aztec
+//! tests.
+
 use anyhow::Result;
 pub mod config;
 pub use config::Config;
 
+/// Generates aztec-noir code from a `.tree` file. Has a uniform interface with the default
+/// solidity backend.
+/// TODO: should we define a Backend trait?
 pub fn scaffold(_text: &String, _cfg: &Config) -> Result<String> {
     Ok("".to_string())
 }
