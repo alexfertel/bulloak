0f754e62bb335f0f262d742ee52f1012cfc7805e
teddy <teddy@defi.sucks>
2025-11-24 19:01:52 -0300

    feat: do not generate revert cases on spurious revert/panic

--- crates/noir/src/constants.rs
+++ crates/noir/src/constants.rs
@@ -3,7 +3,7 @@
 /// Keywords that indicate a test should have the `#[test(should_fail)]`
 /// attribute.
 pub(crate) const PANIC_KEYWORDS: &[&str] =
-    &["panic", "revert", "error", "fail", "assert_fail"];
+    &["it should panic", "it should revert"];
 
 /// Prefix for test functions.
 pub(crate) const TEST_PREFIX: &str = "test";
